19eb63e6558059301eba500d2a11b2ed57a45738
Dmytro Popovskyi <popovskiy96@gmail.com>
2025-05-18 19:27:43 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * 1000000);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Welcome!');
+    } else {
+      console.log('The door is closed');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log('The door is open');
+    } else {
+      console.log('The key is not correct');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +62,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
